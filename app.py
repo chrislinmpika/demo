84889688b6cc45c7fbe7a4f5bacc563100f172ae
chrislinmpika <chris.mpika@icloud.com>
2024-03-03 03:50:36 +0100

    update git write back with branch condition

--- app.py
+++ app.py
@@ -6,14 +6,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    # Get the environment variable and capitalize it
     var_value = os.getenv("ENV", "<ENV is not set>").capitalize()
-    # Add an emoji to the message
-    emoji = "🚀"  # Rocket emoji as an example
+    emoji = "🚀"
     return f"<h1>{emoji} Env is {var_value}</h1>"
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
-
-
