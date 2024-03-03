5369d147c1bcea6a059cb205b767cd5df6879302
chrislinmpika <chris.mpika@icloud.com>
2024-03-03 02:13:51 +0100

    add more Emojis in ev msg

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ def hello():
     # Get the environment variable and capitalize it
     var_value = os.getenv("ENV", "<ENV is not set>").capitalize()
     # Add an emoji to the message
-    emoji = "🚀"  # Rocket emoji as an example
-    return f"<h1>Environment: {var_value} {emoji}</h1>"
+    emoji = "🚀🚀"  # Rocket emoji as an example
+    return f"<h1>{emoji} Environment is {var_value}</h1>"
 
 
 if __name__ == "__main__":
