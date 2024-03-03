03c1fd509215f7da88c0757e4d34cfb880ae4187
chrislinmpika <chris.mpika@icloud.com>
2024-03-03 03:37:44 +0100

    update env msg

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def hello():
     var_value = os.getenv("ENV", "<ENV is not set>").capitalize()
     # Add an emoji to the message
     emoji = "🚀"  # Rocket emoji as an example
-    return f"<h1>{emoji} Environment is {var_value}</h1>"
+    return f"<h1>{emoji} Env is {var_value}</h1>"
 
 
 if __name__ == "__main__":
